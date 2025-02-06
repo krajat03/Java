46359549ec54b5df8bda1395fedb34a6eb985311
krajat03 <rajatunsar@gmail.com>
2025-02-06 15:46:18 +0530

    Pattern Problem

---
+++ Inverted_Numbered_Half_Pyramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Inverted_Numbered_Half_Pyramid {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = s.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
