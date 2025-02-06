181cbeeb2844e32dbf515fb2424af652327193ca
krajat03 <rajatunsar@gmail.com>
2025-02-06 14:45:24 +0530

    Pattern Problem

---
+++ Half_Pyramid.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Half_Pyramid {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = s.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}
