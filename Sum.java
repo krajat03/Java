5a0b0f8effa7a526e2c69d26ef969166d74b9a0d
krajat03 <rajatunsar@gmail.com>
2025-03-12 11:05:34 +0530

    Finding the sum of first n Natural numbers

--- Sum.java
+++ Sum.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-
 public class Sum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the limit: ");
         int limit = scanner.nextInt();
         scanner.close();
-        System.out.println("Natural numbers till " + limit + " are: ");
-        for (int i = 1; i < limit + 1; i++) {
-            System.out.println("-> " + i);
+        int sum = 0;
+        for (int i = 1; i <= limit; i++) {
+            sum += i;
         }
+        System.out.println("-> The sum of first " + limit + " natural numbers are: " + sum);
     }
 }
