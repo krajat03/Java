d2245ae03ed2c2e2be244a91bc591a787b09b97f
krajat03 <rajatunsar@gmail.com>
2025-03-29 18:42:55 +0530

    Using Iterative Approach

---
+++ Factroial.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Factroial {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = s.nextInt();
+        int fact = 1;
+        for (int i = num; i > 0; i--) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial: " + fact);
+        s.close();
+    }
+}
