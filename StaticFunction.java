5aa7a3d3d2c3231d5ae41241f6ddee0118b5ff2a
krajat03 <rajatunsar@gmail.com>
2025-04-24 12:20:33 +0530

    Learn static function

---
+++ StaticFunction.java
@@ -0,0 +1,14 @@
+class StaticFunction {
+    static void fun1(){
+        System.out.println("-> This is function 1");
+    }
+    static void fun2(){
+        System.out.println("-> This is function 2");
+    }
+    public static void main(String[] args) {
+        System.out.println("Calling Functions:- ");
+        fun1();
+        fun2();
+    }
+}
+
