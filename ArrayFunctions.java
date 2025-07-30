b6a5f5cff6a196a17a487a7a6d5081b41142cb94
krajat03 <rajatunsar@gmail.com>
2025-07-30 08:41:10 +0530

    Update fuctions for String array operation

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -5,12 +5,15 @@ public class ArrayFunctions {
         Scanner scanner = new Scanner(System.in);
 
         int[] array = createArray(scanner);
+        String[] str = createStringArray(scanner);
 
         // Take input of elements of array
         arrayInput(array, scanner);
+        arrayStringInput(str, scanner);
 
         // Print array
         arrayOutput(array);
+        arrayStringOutput(str);
 
     }
 
@@ -19,6 +22,13 @@ public class ArrayFunctions {
         System.out.print("Enter size of array: ");
         int size = scanner.nextInt();
         return new int[size];
+    }
+
+    // Create String array
+    public static String[] createStringArray(Scanner scanner) {
+        System.out.print("Enter size of array: ");
+        int size = scanner.nextInt();
+        return new String[size];
     }
 
     // Input function
@@ -30,10 +40,29 @@ public class ArrayFunctions {
         return array;
     }
 
+    // String Input function
+    public static String[] arrayStringInput(String[] array, Scanner scanner) {
+        scanner.nextLine();
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("-> Enter element: ");
+            array[i] = scanner.nextLine();
+        }
+        return array;
+    }
+
     // Output function
     public static void arrayOutput(int[] array) {
         System.out.print("Array is:- \n-> ");
         for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    // String Output function
+    public static void arrayStringOutput(String[] array) {
+        System.out.print("Array is:- \n-> ");
+        for (String i : array) {
             System.out.print(i + " ");
         }
         System.out.println();
