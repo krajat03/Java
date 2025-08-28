709d440dcd4cfdb53a84c45c029e8fe4993fee5c
krajat03 <rajatunsar@gmail.com>
2025-08-28 17:35:18 +0530

    Add recursive print function with base case demonstration

---
+++ Recursion1.java
@@ -0,0 +1,16 @@
+public class Recursion1 {
+    public static void main(String[] args) {
+        print();
+    }
+
+    static int count = 0;
+
+    static void print() {
+        if (count == 3) {
+            return;
+        }
+        System.out.println(count);
+        count++;
+        print();
+    }
+}
