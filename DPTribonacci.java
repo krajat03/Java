bd5e72ea9a14b51feb5b553a3db40ea7aec72893
krajat03 <rajatunsar@gmail.com>
2025-11-07 11:22:54 +0530

    [200~Add DP-based Tribonacci number calculator using memoization

--- DPTribonacci.java
+++ DPTribonacci.java
@@ -18,9 +18,9 @@ public class DPTribonacci {
     }
 
     static int tribonacci(int n, int[] dp) {
-        if (n <= 1)
-            return n;
-        if (n == 2)
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
             return 1;
 
         if (dp[n] != -1) {
